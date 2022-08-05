9cdbf0a14566f5d4053f57fe1137ccc8bb69a4fc
Chaitanya Takalikar <ctakali1@CS444-ctakali1.bu.binghamton.edu>
2022-08-05 04:06:51 +0000

    project 5 initiated again

--- submit/prj5-sol/src/components/app.jsx
+++ submit/prj5-sol/src/components/app.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 
 const { useState } = React;
 
-import makeKnnWsClient from '../knn-ws-client.mjs';
-import canvasToMnistB64 from '../canvas-to-mnist-b64.mjs';
+import makeKnnWsClient from './knn-ws-client.mjs';
 
 const DEFAULT_WS_URL = 'https://zdu.binghamton.edu:2345';
 
 export default function App(props) {
-  return (
-    <div>
-      <button id="my-btn">
-        Classify
-      </button>
-    </div>
-  );
+  //TODO
+  return "";
 }
