3189813b165a2425c1d9b0fa7e6d53765055b0c1
Chaitanya Takalikar <ctakali1@CS444-ctakali1.bu.binghamton.edu>
2022-08-05 19:47:23 +0000

    canvas designed

--- submit/prj5-sol/src/components/app.jsx
+++ submit/prj5-sol/src/components/app.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 const { useState } = React;
 
 import makeKnnWsClient from './knn-ws-client.mjs';
+import Canvas from './Canvas';
 
 const DEFAULT_WS_URL = 'https://zdu.binghamton.edu:2345';
 
 export default function App(props) {
   //TODO
-  return "";
+  return (
+    <div>
+      <Canvas />
+
+    </div>
+  )
 }
